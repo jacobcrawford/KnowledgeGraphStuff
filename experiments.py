8e6eedf708c051c947594a7fd39216d428c7eb07
Jacob Crawford <jacobcrawford1@hotmail.com>
2020-10-19 09:28:01 +0200

    trying to run in parallel

--- experiments.py
+++ experiments.py
@@ -5,10 +5,12 @@ import time
 from os import listdir
 from os.path import isfile, join
 import logging
+import argparse
 
 import numpy as np
 import pandas as pd
 
+from GLIMPSE_personalized_KGsummarization.main import float_in_zero_one
 from GLIMPSE_personalized_KGsummarization.src.algorithms import query_vector, random_walk_with_restart
 from GLIMPSE_personalized_KGsummarization.src.base import DBPedia
 from GLIMPSE_personalized_KGsummarization.src.glimpse import GLIMPSE
@@ -26,14 +28,16 @@ def loadDBPedia(path):
     logging.info('Loaded {}'.format(KG.name()))
     return KG
 
-def printResults():
+def printResults(version):
     path1 = "experiments_results"
     path2 = "experiments_results_pagerank"
 
-    ppr2 = [f for f in listdir(path2) if isfile(join(path2, f)) and f.endswith(".csv") and "PPR#2" in f]
-    ppr5 = [f for f in listdir(path2) if isfile(join(path2, f)) and f.endswith(".csv") and "PPR#5" in f]
-    glimpse1 = [f for f in listdir(path1) if isfile(join(path1, f)) and f.endswith(".csv") and "e#0.01" in f]
-    glimpse2 = [f for f in listdir(path1) if isfile(join(path1, f)) and f.endswith(".csv") and "e#0.001" in f]
+    version_string = version if version is not None else ""
+
+    ppr2 = [f for f in listdir(path2) if isfile(join(path2, f)) and f.endswith(".csv") and "PPR#2" in f and version_string in f]
+    ppr5 = [f for f in listdir(path2) if isfile(join(path2, f)) and f.endswith(".csv") and "PPR#5" in f and version_string in f]
+    glimpse1 = [f for f in listdir(path1) if isfile(join(path1, f)) and f.endswith(".csv") and "e#0.01" in f and version_string in f]
+    glimpse2 = [f for f in listdir(path1) if isfile(join(path1, f)) and f.endswith(".csv") and "e#0.001" in f and version_string in f]
 
     rows = []
     div = 10000
@@ -41,7 +45,6 @@ def printResults():
     print("PPR2")
     def pctf(x):
         f = math.ceil(int(x)/47408000*tms)/div
-        print("F:" + str(f), str(type(f)))
         if f > 1:
             print(f)
             return str(math.floor(f))
@@ -162,7 +165,7 @@ def runGLIMPSEExperiment():
 
     path = sys.argv[1]
     KG = loadDBPedia(path)
-    K = [10*(10**-i)*KG.number_of_triples() for i in range(2, 7)]
+    K = [10*(10**-i) for i in range(2, 7)]
     E = [1e-2]
 
     logging.info("KG entities: " +str(len(KG.entity_id_)))
@@ -217,10 +220,179 @@ def runGLIMPSEExperiment():
 
             pd.DataFrame(rows).to_csv("experiments_results/v"+version+ "T#" +str(KG.number_of_triples())+"_E#"+str(KG.number_of_entities()) +"K#"+str(int(k))+"e#"+str(e)+ ".csv")
 
+def runGLIMPSEExperimentOnce(k, e,version, answers_version, kg_path):
+    path = "user_query_log_answers" + answers_version + "/"
+    user_log_answer_files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(".csv")]
+    number_of_users = len(user_log_answer_files)
+
+    user_log_train = []
+    user_log_test = []
+    user_answers = []
+    user_ids = []
+
+    for file in user_log_answer_files:
+        df = pd.read_csv(path + str(file))
+        user_ids.append(file.split(".csv")[0])
+        # list of lists of answers as iris
+        user_answers.append([["<" + iri + ">" for iri in f.split(" ")] for f in df['answers']])
+
+
+    KG = loadDBPedia(kg_path)
+
+    logging.info("KG entities: " + str(len(KG.number_of_entities())))
+    logging.info("KG triples: " + str(KG.number_of_triples_))
+
+    for i in range(number_of_users):
+        # Split log in 70%
+        split_index_train = int(len(user_answers[i]) * 0.7)
+
+        user_log_train.append([[entity for entity in answers_to_query if KG.is_entity(entity)]
+                               for answers_to_query in user_answers[i][:split_index_train]])
+        user_log_test.append([[entity for entity in answers_to_query if KG.is_entity(entity)]
+                              for answers_to_query in user_answers[i][split_index_train:]])
+
+
+    logging.info("Running for K=" + str(k) + ", e=" + str(e))
+    rows = []
+    for idx_u in range(number_of_users):
+        KG.reset()
+        # model user pref
+        logging.info("  Running GLIMPSE on user: " + user_ids[idx_u])
+        t1 = time.time()
+        summary = GLIMPSE(KG, k, user_log_train[idx_u], e)
+        logging.info("  Done")
+        t2 = time.time()
+        total_count = 0
+        total_entities = 0
+
+        accuracies = []
+        for answers_to_query in user_log_test[idx_u]:
+            count = 0
+            total_answers = len(answers_to_query)
+            if total_answers == 0:
+                continue
+            else:
+                total_entities += total_answers
+                for iri in answers_to_query:
+                    if summary.has_entity(iri):
+                        count += 1
+                        total_count += 1
+                accuracies.append(count / total_answers)
+
+        mean_accuracy = np.mean(np.array(accuracies))
+        logging.info("      Summary  accuracy " + str(mean_accuracy) + "%")
+        rows.append({'match': total_count, 'total': total_entities, '%': mean_accuracy, 'runtime': t2 - t1})
+
+    pd.DataFrame(rows).to_csv("experiments_results/v" + version + "T#" + str(KG.number_of_triples()) + "_E#" + str(
+        KG.number_of_entities()) + "K#" + str(int(k)) + "e#" + str(e) + ".csv")
+
+def pageRankExperimentOnce(k,ppr,version,answers_version, kg_path):
+    KG = loadDBPedia(kg_path)
+    path = "user_query_log_answers" + answers_version + "/"
+    user_log_answer_files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(".csv")]
+    number_of_users = len(user_log_answer_files)
+
+    user_log_train = []
+    user_log_test = []
+    user_answers = []
+
+    for i in range(number_of_users):
+
+        for file in user_log_answer_files:
+            df = pd.read_csv(path + str(file))
+            # list of lists of answers as iris
+            user_answers.append([["<" + iri + ">" for iri in f.split(" ")] for f in df['answers']])
+
+        # Split log in 70%
+        split_index_train = int(len(user_answers[i]) * 0.7)
+
+        # collapse to one list of entities
+        user_log_train.append([f for c in user_answers[i][:split_index_train] for f in c if KG.is_entity(f)])
+        user_log_test.append([f for c in user_answers[i][split_index_train:] for f in c if KG.is_entity(f)])
+
+    rows = []
+    for i in range(number_of_users):
+        t1 = time.time()
+        qv = query_vector(KG, user_log_train[i])
+        M = KG.transition_matrix()
+        ppr_v = random_walk_with_restart(M, qv, 0.15, ppr)
+
+        t2 = time.time()
+
+        # Extract k indexes
+        indexes = np.argpartition(ppr_v, -k)[-k:]
+        summary = [KG.id_entity(i) for i in indexes]
+
+        count = 0
+        total = len(user_log_test[i])
+        for iri in user_log_test[i]:
+            if iri in summary:
+                count += 1
+        rows.append({'match': count, 'total': total, '%': count / total, 'runtime': t2 - t1})
+    pd.DataFrame(rows).to_csv(
+        "experiments_results_pagerank/v" + version + "T#" + str(KG.number_of_triples()) + "_E#" + str(
+            KG.number_of_entities()) + "_K#" + str(int(k)) + "_PPR#" + str(ppr) + ".csv")
+
 
 def f1skew(fn):
     return (2/(1 + fn))/(1+(1/(1+fn)))
 
-path = sys.argv[1]
-#pageRankExperiment(path)
-runGLIMPSEExperiment()
+METHODS = {
+    'glimpse'
+    'ppr'
+}
+
+VERSIONS = {
+    'v2': 'More users',
+    'v3': 'Normalizing query vector'
+}
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--KG-path', default='../dpbedia3.9/', help='Path to the KG files')
+
+    parser.add_argument('--percent-triples', type=float_in_zero_one, default=0.001,
+            help='Ratio of number of triples of KG to use as K '
+                 '(summary constraint). Default is 0.001.')
+    parser.add_argument('--walk', help="length of ppr walk")
+    parser.add_argument('--version', help='version of experiments', choices=list(VERSIONS.keys()))
+    parser.add_argument('--version-answers', help='version of extracted user answers')
+
+
+    parser.add_argument('--epsilon',
+            help='Set this flag to the epsilon parameter.Defines the sampling size.', default=1e-2)
+
+    parser.add_argument('--method', nargs='+', default=['glimpse'],
+            choices=list(METHODS),
+            help='Experiments to run')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    KG = loadDBPedia(args.KG_path)
+
+    # Load the KG into memory
+    logging.info('Loading {}'.format(KG.name()))
+    KG.load()
+    logging.info('Loaded {}'.format(KG.name()))
+
+    k = args.percent_triples
+    version = args.version
+    answer_version = args.version_answers
+    kg_path = args.KG_path
+
+    if args.method == 'glimpse':
+        e = args.epsilon
+        runGLIMPSEExperimentOnce(k,e,version, answer_version, kg_path)
+    if args.method == 'ppr':
+        ppr = args.walk
+        pageRankExperimentOnce(k,ppr,version,answer_version, kg_path )
+
+
+if __name__ == '__main__':
+    main()
---
+++ parallel_experiments.py
@@ -0,0 +1,30 @@
+K = [10*(10**-i) for i in range(2, 7)]
+E = [1e-2]
+import os
+
+answer_version = "v2"
+version = "v3"
+
+if (False):
+    for k in K:
+        for e in E:
+            os.system('nohup python experiments.py'
+                      + ' --method glimpse'
+                      + ' --persent-triples ' + str(k)
+                      + ' --version ' + version
+                      + ' --version ' + answer_version
+                      + ' --epsilon ' + str(e)
+                      + ' > ' + 'GLIMPSE' + version+'a' + answer_version + ' #K' + str(k) + '#E' + str(e) + '.out'
+                      + ' &')
+
+for k in K:
+    for ppr in [2,5]:
+        os.system('nohup python experiments.py'
+                  + ' --method ppr'
+                  + ' --walk '+str(ppr)
+                  + ' --persent-triples ' + str(k)
+                  + ' --version ' + version
+                  + ' --version ' + answer_version
+                  + ' > ' + 'PPR'+str(ppr) + version+'a' + answer_version + ' #K' + str(k) + '.out'
+                  + ' &')
+        exit(1)
